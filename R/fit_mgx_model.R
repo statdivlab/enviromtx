79d9fea7455781dcfb0715f27a1d9cacd104847e
amy <amy.willis7@gmail.com>
2025-08-26 13:45:48 -0700

    robustify to neg and inf values

--- R/fit_mgx_model.R
+++ R/fit_mgx_model.R
@@ -7,15 +7,15 @@
 #' \eqn{X_{ij}} refers to the observed abundance of taxon \eqn{j} in sample \eqn{i},
 #' \eqn{X_{ij^*}} refers to the observed abundance of taxon \eqn{j^*} in sample \eqn{i}, and
 #' \eqn{W_{im}} is the value of abiotic covariate \eqn{m} observed in sample \eqn{i}.
-#' We can interpret \eqn{1.01^{\beta_1}} as the multiplicative change in the expression-per-unit-coverage of gene \eqn{k} in species \eqn{j} for 1\\% increase in the coverage of species \eqn{j} compared to species \eqn{j^*}. With some slightly stronger assumptions about the sampling mechanism, we can also interpret this on the true cell abundance scale (rather than just the coverage scale).
+#' We can interpret \eqn{1.01^{\beta_1}} as the multiplicative change in the expression-per-unit-coverage of gene \eqn{k} in species \eqn{j} for 1\\% increase in the coverage of species \eqn{j} compared to species \eqn{j^*}, when comparing samples with the same values of the abiotic covariates. With some slightly stronger assumptions about the sampling mechanism, we can also interpret this on the true cell abundance scale (rather than just the coverage scale).
 #'
 #' @param enviro_df A data frame or tibble with columns containing the relevant variables, and rows denoting the observations. Columns must contain the following data: observed gene expression data, taxon abundance data, environmental covariates, and (optionally) technical replicate information.
 #' @param yy The name of the column in `enviro_df` containing the expression data for a gene expressed by the responder taxon. Default is "yy". Expression data could be in the form of coverage, counts, etc. See vignettes for details on acceptable and suggested datatypes and preprocessing ("normalizations"). In the above notation, this corresponds to the expression of gene \eqn{k} expressed by taxon \eqn{j}.
 #' @param xx The name of the column in `enviro_df` containing abundances of the responder taxon. Default is "xx". In the above notation, this corresponds to the abundance of taxon \eqn{j}.
 #' @param xstar The name of the column in `enviro_df` containing abundances of the companion taxon. Default is "xstar". In the above notation, this corresponds to the abundance of  taxon \eqn{j^*}.
-#' @param replicates The name of the column in `enviro_df` containing technical replicate information. Entries in this column should be the same within observations that are technical replicates. Will be coerced to a factor internally. Default is `NULL`.
+#' @param replicates The name of the column in `enviro_df` containing technical replicate information. Entries in this column should be the same within observations that are technical replicates. Will be coerced to a factor internally. Default is `NULL` (no samples have technical replicates; all samples are independent).
 #' @param formula A formula describing the environmental covariates that should be included in the model. The variable names should be columns in `enviro_df`
-#' @param wts The name of the column in `enviro_df` containing nonnegative weights for the observation. Could be sequencing depth to put more emphasis on deeply sequenced samples. Default is `NULL`.
+#' @param wts The name of the column in `enviro_df` containing nonnegative weights for the observation. Could be sequencing depth to put more emphasis on deeply sequenced samples. Default is `NULL` (all observations contribute equally to the likelihood).
 #' @param replace_zeros What to replace zeros with in the denominator \eqn{X_{ij}}. Options include "minimum" (replace with the smallest value) or pseudocount numeric value (eg. 1). Default is "minimum".
 #' @param use_jack_se Boolean indicating whether to use jackknife standard errors (TRUE) rather than sandwich standard errors (FALSE). Only relevant if technical replicates are included. Jackknife standard errors are recommended when
 #' there are a small number of observations. Defaults to FALSE.
@@ -104,6 +104,11 @@ fit_mgx_model <- function(
   if (any(enviro_df[[yy]] < 0)) stop("You gave negative expression data `yy`. This doesn't make sense.")
 
 
+  if (any(is.infinite(enviro_df[[xstar]]))) stop("You gave infinite abundance data for `xstar`. This doesn't make sense.")
+  if (any(is.infinite(enviro_df[[xx]]))) stop("You gave infinite abundance data for `xx`. This doesn't make sense.")
+  if (any(is.infinite(enviro_df[[yy]]))) stop("You gave infinite abundance data for `yy`. This doesn't make sense.")
+
+
   ################################################
   ## set up the data for fitting
   ################################################
@@ -125,11 +130,11 @@ fit_mgx_model <- function(
     my_df <- dplyr::bind_cols(tibble::tibble(predictor), enviro_df)
 
   }
-
-  if(any(apply(my_df, 2, is.infinite))) {
-    print(my_df, n = nrow(my_df))
-    stop("Infinities in my_df? Amy's fault.")
-  }
+#
+#   if(any(apply(my_df, 2, is.infinite))) {
+#     print(my_df, n = nrow(my_df))
+#     stop("Infinities in my_df? Amy's fault.")
+#   }
 
   if (is.null(wts)) {
     my_df$wts <- rep(1L, nrow(my_df))
